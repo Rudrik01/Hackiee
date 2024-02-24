229d8fa19ab4ad3ab120aeed5b59eb93dfbb206f
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2024-02-24 22:52:25 +0530

    Initital Commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import google.generativeai as genai
+from pathlib import Path
+import gradio as gr
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Configure the GenerativeAI API key using the loaded environment variable
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Set up the model configuration for text generation
+generation_config = {
+    "temperature": 0.4,
+    "top_p": 1,
+    "top_k": 32,
+    "max_output_tokens": 4096,
+}
+
+# Define safety settings for content generation
+safety_settings = [
+    {"category": f"HARM_CATEGORY_{category}", "threshold": "BLOCK_MEDIUM_AND_ABOVE"}
+    for category in ["HARASSMENT", "HATE_SPEECH", "SEXUALLY_EXPLICIT", "DANGEROUS_CONTENT"]
+]
+
+# Initialize the GenerativeModel with the specified model name, configuration, and safety settings
+model = genai.GenerativeModel(
+    model_name="gemini-pro-vision",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
+# Function to read image data from a file path
+def read_image_data(file_path):
+    image_path = Path(file_path)
+    if not image_path.exists():
+        raise FileNotFoundError(f"Could not find image: {image_path}")
+    return {"mime_type": "image/jpeg", "data": image_path.read_bytes()}
